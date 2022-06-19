3e797dd288690388a8221cb77454f78af1b64df6
yaxan <ymasoud@uwaterloo.ca>
2022-06-19 16:17:14 -0400

    Changed augmentations

--- train.py
+++ train.py
@@ -2,25 +2,26 @@ import os
 import numpy as np
 
 import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import *
-from tensorflow.keras import models
+from tensorflow.keras import layers, models
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
 from tensorflow.keras.optimizers import RMSprop,Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
 from tensorflow.python.distribute.collective_all_reduce_strategy import CollectiveAllReduceExtended
 CollectiveAllReduceExtended._enable_check_health = False
 
 tf.compat.v1.disable_eager_execution()
-PATH_TO_TRAIN_DATA = "D:\\code\\Naruto_Handsign_Classification\\TrainDataa"
+PATH_TO_TRAIN_DATA = "D:\\code\\Naruto_Handsign_Classification\\train"
 PATH_TO_TEST_DATA = "D:\\code\\Naruto_Handsign_Classification\\Data"
 BATCH_SIZE = 16
-TRAIN_SIZE = 3318
+TRAIN_SIZE = 5077
 TEST_SIZE = 290
-tf.random.set_seed(42)
-np.random.seed(42)
 
 #Descriptor of file structure of a dataset (example dataset_train)
 #./dataset
@@ -35,14 +36,13 @@ if __name__ ==  '__main__':
           datagen = ImageDataGenerator(
                               rescale=1./255,
                               featurewise_center=False,
-                              featurewise_std_normalization=False,
+                              featurewise_std_normalization=True,
                               rotation_range=25,
-                              width_shift_range=0.2,
-                              height_shift_range=0.2,
+                              width_shift_range=0.3,
+                              height_shift_range=0.3,
                               shear_range=0.10,
-                              zoom_range=0.15,
-                              horizontal_flip=True,
-                              fill_mode="nearest")
+                              zoom_range=0.3,
+                              horizontal_flip=False)
       else:
           datagen = ImageDataGenerator(rescale=1./255)
 
@@ -54,23 +54,25 @@ if __name__ ==  '__main__':
               class_mode='categorical',
               batch_size=BATCH_SIZE)
 
-  train_generator  = get_datagen(PATH_TO_TRAIN_DATA, False)
+  train_generator  = get_datagen(PATH_TO_TRAIN_DATA, True)
   test_generator   = get_datagen(PATH_TO_TEST_DATA, False)
 
   def load_model(m):
     if m == 'MN':
       model = tf.keras.applications.mobilenet_v2.MobileNetV2(
       input_shape=(224,224,3), alpha=1.0, include_top=False, weights='imagenet', pooling=None)
-
-      for i in range(135):
-       model.layers[i].trainable = False
+      
+      for layer in model.layers[:-1]:
+        layer.trainable = False
+      #for i in range(150):
+       #model.layers[i].trainable = False
 
       mobile_net = Flatten()(model.output)
-      mobile_net = Dropout(0.5)(mobile_net)
+      mobile_net = Dropout(0.3)(mobile_net)
       mobile_net = Dense(4096, activation='relu')(mobile_net)
-      mobile_net = Dropout(0.5)(mobile_net)
+      mobile_net = Dropout(0.3)(mobile_net)
       mobile_net = Dense(1024, activation='relu')(mobile_net)
-      mobile_net = Dropout(0.5)(mobile_net)
+      mobile_net = Dropout(0.3)(mobile_net)
       mobile_net = Dense(12, activation='softmax')(mobile_net)
 
       mobile_net_mobile = Model(model.input, mobile_net, name='Altered_MobileNet')
@@ -79,33 +81,8 @@ if __name__ ==  '__main__':
       model = mobile_net_mobile
 
     else:
-      model = tf.keras.applications.resnet50.ResNet50(
-        include_top=False,
-        weights='imagenet',
-        input_tensor=None,
-        input_shape=(224, 224, 3),
-        pooling=None,
-        classes=12,
-      )
-      model.summary()
-      # model = tf.keras.models.load_model("squeezenet")
-      # model.summary()
+      train_ds = preprocess_input(train_generator) 
 
-      # for i in range(53):
-      #   model.layers[i].trainable = False
-
-      # squeezenet = Flatten()(model.layers[-2].output)
-      # squeezenet = Dropout(0.5)(squeezenet)
-      # squeezenet = Dense(4096, activation='relu')(squeezenet)
-      # squeezenet = Dropout(0.5)(squeezenet)
-      # squeezenet = Dense(1024, activation='relu')(squeezenet)
-      # squeezenet = Dropout(0.5)(squeezenet)
-      # squeezenet = Dense(12, activation='softmax')(squeezenet)
-
-      # squeezenet_out = Model(model.input, squeezenet, name='Altered_SqueezeNet')
-      # squeezenet_out.summary()
-
-      # model = squeezenet_out
     return model
 
   model = load_model('MN')
@@ -113,7 +90,7 @@ if __name__ ==  '__main__':
   adam = tf.keras.optimizers.Adam(learning_rate=0.001)
   sgd = tf.keras.optimizers.SGD(learning_rate=0.001)
   rlrop = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_accuracy',mode='max',factor=0.5, patience=10, min_lr=0.001, verbose=1)
-  early_stopper = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=1,
+  early_stopper = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=20, verbose=1,
                                             mode='auto', baseline=None, restore_best_weights=False)
   model.compile(loss='categorical_crossentropy',
                   optimizer=adam, metrics=['accuracy'])
